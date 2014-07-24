a89970d78db5b5593d7891bb1dfd1a75bdb710f1
Davio Cianci <dccianci@gmail.com>
2014-07-24 12:56:45 -0500

    Added fixed dm2 slice to Chi2 surface - Still WIP

--- Sens/CL_Comparison/Chi2_Surface.C
+++ Sens/CL_Comparison/Chi2_Surface.C
@@ -4,6 +4,7 @@
 #include "TH1F.h"
 #include "TString.h"
 #include "TGraph.h"
+#include "TMultiGraph.h"
 #include "TGaxis.h"
 #include "TNtuple.h"
 #include "TChain.h"
@@ -29,73 +30,145 @@
 
 int multiple_detector_fit()
 {
+/* ------------------------------ */
+
+  // Fix DM2 point for slice plot (otherwise set to negative)
+  int fixedDM = -1;
+
+  bool shape_only = false;
+
+  // Select one of the following modifiers
+  bool statmatch = false;
+  bool noflux_statmatch = false;
+  bool full = true;
+
+  // Select any (or all) detector setups
+  // Note that multiple choices are only really significant for a fixed DM, since they'll all meld together in a 3d model
+  bool use100 = true;
+  bool use150 = false;
+  bool use200 = false; 
+
+/* ------------------------------ */
 
 
-  //Choose input file you want
-  TFile *f = new TFile("output/chi2_Surface_LAr1ND_100m_T600_ShapeOnly.root");
+  TNtuple * thisNtuple[3];
+  int counter = 0;
 
-  //Read in the ntuple that you want to study
-  TNtuple * thisNtuple;
-  f->GetObject("chi2", thisNtuple);
-  
+
+  // First, build the filename and import the file(s)
+  std::string shapeName;
+  std::string mod;
+
+  if(shape_only)	shapeName = "ShapeOnly";
+  else    shapeName = "Shape_and_Rate";
+
+  if(statmatch)    mod = "_StatMatch.root";
+  else if(noflux_statmatch)    mod = "_NoFlux_StatMatch.root";
+  else if(full)    mod = ".root";
+
+  if(use100){
+    std::string name = "output/chi2_Surface_LAr1ND_100m_T600_";
+    name = name + shapeName + mod;
+    TFile *f = new TFile(name.c_str());	
+    f->GetObject("chi2",thisNtuple[counter]);
+    counter ++;
+  }
+  if(use150){
+    std::string name = "output/chi2_Surface_LAr1ND_150m_T600_";
+    name = name + shapeName + mod;
+    TFile *f = new TFile(name.c_str());	
+    f->GetObject("chi2",thisNtuple[counter]);
+    counter ++;
+  }
+  if(use200){
+    std::string name = "output/chi2_Surface_LAr1ND_200m_T600_";
+    name = name + shapeName + mod;
+    TFile *f = new TFile(name.c_str());	
+    f->GetObject("chi2",thisNtuple[counter]);
+    counter ++;
+  }
+ 
   Float_t dm2_temp;
   Float_t sin22th_temp;
   Float_t chi2_temp;
 
-  int npoints = 500;
-
-  // configure the dm2, sin22th points:
-  double dm2min(0.01), dm2max(100.0);
-  double sin22thmin(0.001), sin22thmax(1.0);  
-
   //extract the values from the ntuple
-  //The values are tied together as an entry such that each point has a dm2, sin22theta, and 
-  thisNtuple->SetBranchAddress("chisq", &chi2_temp);
-  thisNtuple->SetBranchAddress("dm2", &dm2_temp);
-  thisNtuple->SetBranchAddress("sin22th", &sin22th_temp);
+  int i_entry;
 
-  int i_entry = 0;
-  int i_dm2 = 0;
-  int i_sin22th = 0;
+  // Initialize the surface plots
+  TGraph2D *g[3];
+  TGraph2D *ThreeSigma[3];
+  TGraph2D *OneSigma[3];
 
-  std::cout<< "Entries : " << thisNtuple->GetEntries() << std::endl;
+  // Initialize the cross sectional plots
+  TGraph *g2D[3];
+  TGraph *ThreeSigma2D[3];
+  TGraph *OneSigma2D[3];
+
+
+
+for(int i = 0; i < counter; i++)				// Cycle through each detector setup
+{
+  thisNtuple[i]->SetBranchAddress("chisq", &chi2_temp);
+  thisNtuple[i]->SetBranchAddress("dm2", &dm2_temp);
+  thisNtuple[i]->SetBranchAddress("sin22th", &sin22th_temp);
+
+  if(fixedDM > 0)
+  {
+    g2D[i] = new TGraph();
+    ThreeSigma2D[i] = new TGraph();
+    OneSigma2D[i] = new TGraph();
+  }
+
+  g[i] = new TGraph2D();
+  ThreeSigma[i] = new TGraph2D();
+  OneSigma[i] = new TGraph2D();
   
-  TGraph2D *g = new TGraph2D();
-  TGraph2D *ThreeSigma = new TGraph2D();
-  TGraph2D *OneSigma = new TGraph2D();
+  i_entry = 0;
 
-  //Cycle through each entry in the ntuple
-  while (i_entry < thisNtuple->GetEntries())
+  std::cout<< "Entries : " << thisNtuple[i]->GetEntries() << std::endl;
+
+  while (i_entry < thisNtuple[i]->GetEntries())			// Cycle through all entries
     {
 
       //select a point in the ntuple
-      thisNtuple->GetEntry(i_entry);
+      thisNtuple[i]->GetEntry(i_entry);
 
       //store them in the TGraph for plotting
-      if(sqrt(chi2_temp) > 3){
-	ThreeSigma->SetPoint(i_entry, sin22th_temp, dm2_temp, 3);
-      }
-      else{
-        ThreeSigma->SetPoint(i_entry, sin22th_temp, dm2_temp, sqrt(chi2_temp));
-      }
+      if(sqrt(chi2_temp) > 3)	ThreeSigma[i]->SetPoint(i_entry, sin22th_temp, dm2_temp, 3);
+      else	ThreeSigma[i]->SetPoint(i_entry, sin22th_temp, dm2_temp, sqrt(chi2_temp));
 
-      if(sqrt(chi2_temp) > 1){
-	OneSigma->SetPoint(i_entry, sin22th_temp, dm2_temp, 1);
-      }
-      else{
-        OneSigma->SetPoint(i_entry, sin22th_temp, dm2_temp, sqrt(chi2_temp));
-      }    
- 
-      if(sqrt(chi2_temp) > 5){    //cut off at 5\sigma for presentation
-      	g->SetPoint(i_entry, sin22th_temp, dm2_temp, sqrt(25));
-      }
-      else{
-        g->SetPoint(i_entry, sin22th_temp, dm2_temp, sqrt(chi2_temp));
+      if(sqrt(chi2_temp) > 1)	OneSigma[i]->SetPoint(i_entry, sin22th_temp, dm2_temp, 1);
+      else	OneSigma[i]->SetPoint(i_entry, sin22th_temp, dm2_temp, sqrt(chi2_temp));
+
+      if(sqrt(chi2_temp) > 5)	g[i]->SetPoint(i_entry, sin22th_temp, dm2_temp, sqrt(25));
+      else	g[i]->SetPoint(i_entry, sin22th_temp, dm2_temp, sqrt(chi2_temp));
+
+      if(fixedDM > 0 && dm2_temp == fixedDM){	
+
+	if(sqrt(chi2_temp) > 3)	ThreeSigma2D[i]->SetPoint(i_entry, sin22th_temp, 3);
+        else	ThreeSigma2D[i]->SetPoint(i_entry, sin22th_temp, sqrt(chi2_temp));
+
+        if(sqrt(chi2_temp) > 1)	OneSigma2D[i]->SetPoint(i_entry, sin22th_temp, 1);
+        else	OneSigma2D[i]->SetPoint(i_entry, sin22th_temp, sqrt(chi2_temp));
+
+        if(sqrt(chi2_temp) > 5)	g2D[i]->SetPoint(i_entry, sin22th_temp, 5);
+        else	g2D[i]->SetPoint(i_entry, sin22th_temp, sqrt(chi2_temp));
       }
 
       i_entry++;
     }
-    
+}
+//cout << "Counter = " << counter;
+//cout << "...[0] points = " << g2D[0]->GetN();
+//cout << "...[1] points = " << g2D[1]->GetN();
+//cout << "...[2] points = " << g2D[2]->GetN();
+
+
+
+
+  // Draw the surface plot
+  if(fixedDM < 0 || counter == 1){
     std::cout << "Drawing ..." << std::endl; 
     gStyle->SetOptStat(0000);
     gStyle->SetOptFit(0000);
@@ -114,42 +187,99 @@ int multiple_detector_fit()
     c1->SetLogx();
     c1->cd();
     
-    OneSigma->SetTitle("");
-    OneSigma->GetXaxis()->SetTitle("sin^{2}2#theta#lower[0.4]{#mu#kern[-0.3]{#mu}}");
-    OneSigma->GetXaxis()->CenterTitle(true);
-    OneSigma->GetXaxis()->SetLabelFont(62);
-    OneSigma->GetXaxis()->SetLabelOffset(0.003);
-    OneSigma->GetXaxis()->SetLabelSize(0.03);
-    OneSigma->GetXaxis()->SetTitleSize(0.05);
-    OneSigma->GetXaxis()->SetTitleFont(62);
-    OneSigma->GetXaxis()->SetTitleOffset(1.75);
+    OneSigma[0]->SetTitle("");
+    OneSigma[0]->GetXaxis()->SetTitle("sin^{2}2#theta#lower[0.4]{#mu#kern[-0.3]{#mu}}");
+    OneSigma[0]->GetXaxis()->CenterTitle(true);
+    OneSigma[0]->GetXaxis()->SetLabelFont(62);
+    OneSigma[0]->GetXaxis()->SetLabelOffset(0.003);
+    OneSigma[0]->GetXaxis()->SetLabelSize(0.03);
+    OneSigma[0]->GetXaxis()->SetTitleSize(0.05);
+    OneSigma[0]->GetXaxis()->SetTitleFont(62);
+    OneSigma[0]->GetXaxis()->SetTitleOffset(1.75);
     
-    OneSigma->GetYaxis()->SetTitle("#Deltam_{41}^{2} [eV^{2}]");
-    OneSigma->GetYaxis()->CenterTitle(true);
-    OneSigma->GetYaxis()->SetLabelFont(62);
-    OneSigma->GetYaxis()->SetLabelSize(0.03);
-    OneSigma->GetYaxis()->SetTitleSize(0.05);
-    OneSigma->GetYaxis()->SetTitleFont(62);
-    OneSigma->GetYaxis()->SetTitleOffset(2.0);
+    OneSigma[0]->GetYaxis()->SetTitle("#Deltam_{41}^{2} [eV^{2}]");
+    OneSigma[0]->GetYaxis()->CenterTitle(true);
+    OneSigma[0]->GetYaxis()->SetLabelFont(62);
+    OneSigma[0]->GetYaxis()->SetLabelSize(0.03);
+    OneSigma[0]->GetYaxis()->SetTitleSize(0.05);
+    OneSigma[0]->GetYaxis()->SetTitleFont(62);
+    OneSigma[0]->GetYaxis()->SetTitleOffset(2.0);
     
-    OneSigma->GetZaxis()->SetTitle("Confidence Level [#sigma]");
-    OneSigma->GetZaxis()->CenterTitle(true);
-    OneSigma->GetZaxis()->SetLabelFont(62);
-    OneSigma->GetZaxis()->SetLabelSize(0.03);
-    OneSigma->GetZaxis()->SetTitleSize(0.05);
-    OneSigma->GetZaxis()->SetTitleFont(62);
-    OneSigma->GetZaxis()->SetNdivisions(506);
-    OneSigma->GetZaxis()->SetTitleOffset(1.25);
-    OneSigma->GetZaxis()->SetRangeUser(0.0000001, 5.5);
+    OneSigma[0]->GetZaxis()->SetTitle("Confidence Level [#sigma]");
+    OneSigma[0]->GetZaxis()->CenterTitle(true);
+    OneSigma[0]->GetZaxis()->SetLabelFont(62);
+    OneSigma[0]->GetZaxis()->SetLabelSize(0.03);
+    OneSigma[0]->GetZaxis()->SetTitleSize(0.05);
+    OneSigma[0]->GetZaxis()->SetTitleFont(62);
+    OneSigma[0]->GetZaxis()->SetNdivisions(506);
+    OneSigma[0]->GetZaxis()->SetTitleOffset(1.25);
+    OneSigma[0]->GetZaxis()->SetRangeUser(0.0000001, 5.5);
+
+    for(int j = 0; j < counter; j ++)
+    {
+      OneSigma[j]->SetMarkerColor(kRed);
+      if(j == 0) OneSigma[0]->Draw("P");
+      else OneSigma[j]->Draw("Psame");
+    
+      ThreeSigma[j]->SetMarkerColor(kBlue);
+      ThreeSigma[j]->Draw("Psame");
+
+      g[j]->Draw("Psame");
+    }
+  }
 
 
-    OneSigma->SetMarkerColor(kRed);
-    OneSigma->Draw("P");
-    
-    ThreeSigma->SetMarkerColor(kBlue);
-    ThreeSigma->Draw("Psame");
+if(fixedDM > 0){
 
-    g->Draw("Psame");
+  TCanvas *c2 = new TCanvas("c2","Fixed DM2",800,800);
+
+  TMultiGraph *mg = new TMultiGraph();
+
+  c2->SetLeftMargin(.15);
+  c2->SetBottomMargin(.15);
+  c2->SetTopMargin(.05);
+  c2->SetRightMargin(.05);
+  c2->SetLogx();
+  c2->cd();
+
+  OneSigma2D[0]->SetTitle("");
+  OneSigma2D[0]->GetXaxis()->SetTitle("sin^{2}2#theta#lower[0.4]{#mu#kern[-0.3]{#mu}}");
+  OneSigma2D[0]->GetXaxis()->CenterTitle(true);
+  OneSigma2D[0]->GetXaxis()->SetLabelFont(62);
+  OneSigma2D[0]->GetXaxis()->SetLabelOffset(0.003);
+  OneSigma2D[0]->GetXaxis()->SetLabelSize(0.03);
+  OneSigma2D[0]->GetXaxis()->SetTitleSize(0.05);
+  OneSigma2D[0]->GetXaxis()->SetTitleFont(62);
+  OneSigma2D[0]->GetXaxis()->SetTitleOffset(1.75);
+
+  OneSigma2D[0]->GetYaxis()->SetTitle("Confidence Level [#sigma]");
+  OneSigma2D[0]->GetYaxis()->CenterTitle(true);
+  OneSigma2D[0]->GetYaxis()->SetLabelFont(62);
+  OneSigma2D[0]->GetYaxis()->SetLabelSize(0.03);
+  OneSigma2D[0]->GetYaxis()->SetTitleSize(0.05);
+  OneSigma2D[0]->GetYaxis()->SetTitleFont(62);
+  OneSigma2D[0]->GetYaxis()->SetNdivisions(506);
+  OneSigma2D[0]->GetYaxis()->SetTitleOffset(1.25);
+  OneSigma2D[0]->GetYaxis()->SetRangeUser(0.0000001, 5.5);
+
+cout << "drawing...";
+
+
+    OneSigma2D[0]->SetLineColor(kRed);
+    mg->Add(OneSigma2D[0]);
+    ThreeSigma2D[0]->SetLineColor(kBlue);
+    mg->Add(ThreeSigma2D[0]);
+
+    for(int j = 0; j < counter; j ++)
+    {
+      if(j == 1) g2D[j]->SetLineWidth(2);
+      if(j == 2) g2D[j]->SetLineWidth(3);
+      mg->Add(g2D[j]);
+    }
+    
+    mg->Draw("AL");
+
+  }
 
     return 0;
     
